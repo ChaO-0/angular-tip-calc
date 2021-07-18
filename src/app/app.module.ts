540bccc6471d86a6bc160213c6ae9ae9053d4260
Christopher Yu <christophergin88@gmail.com>
2021-07-18 19:11:12 +0800

    feat: new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
 import { CalculatorOutputComponent } from './components/calculator-output/calculator-output.component';
+import { CalculatorInputComponent } from './components/calculator-input/calculator-input.component';
 
 @NgModule({
-  declarations: [AppComponent, CalculatorComponent, CalculatorOutputComponent],
+  declarations: [AppComponent, CalculatorComponent, CalculatorOutputComponent, CalculatorInputComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
